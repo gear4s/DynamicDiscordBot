d6f0de502b73a7db4dc053052901505ba11bb018
Aaron Marais <aaronleem@gmail.com>
2017-10-21 20:24:01 +0200

    added request, removed blizzard api, changed a bunch of things

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-const blizzard = require('blizzard.js').initialize({ apikey: 'api-key' });
 const safeEval = require('notevil')
 const Discord = require("discord.js");
 const clone = require("clone")
 const client = new Discord.Client();
-
 
 process.on('unhandledRejection', (reason) => {
   console.error(reason);
@@ -11,45 +9,70 @@ process.on('unhandledRejection', (reason) => {
 });
 
 var discmd = {}
-var includes = {}
+var includes = {
+  cmd: {},
+  mod: {}
+}
 
+var ready = false
 client.on("ready", () => {
   console.log("Logged in!");
   var chans = client.channels.array()
 
+  var t = 0, j = 0;
   for(var i = 0; i < chans.length; i++) {
-    var chan = chans[i];
-    if(chan.name.split("-")[0] == "cmd") {
-      cmd = chan.name.split("-")[1]
-      discmd[cmd] = {
-        requires: {}
-      }
-      chan.fetchMessages()
-          .then(messages => {
-              console.log(`Received ${messages.size} messages`)
-              messages = messages.array()
-              var i = messages.length
-              while(i--) {
-                var msg = messages[i].content.split("```")
-                discmd[cmd][msg[0]] = msg[1].substring(msg[1].substring(0,10) == "javascript" ? 11 : 0)
-              }
-          })
-          .catch(console.error);
-    } else if(chan.name.split("-")[0] == "mod") {
-      cmd = chan.name.split("-")[1]
-      chan.fetchMessages()
-          .then(messages => {
-              console.log(`Received ${messages.size} messages`)
-              messages = messages.array()
-              var msg = messages[0].content.split("\n")
-              for(var i in msg) {
-                var code = msg[i].split(":")
-                includes[code[1]] = require(code[1])
-                discmd[cmd].requires[code[0]] = includes[code[1]]
-              }
+    if(chans[i].type == "text") {
+      j++
+      chans[i]
+        .fetchMessages()
+        .then(
+          msgs => {
+            var messages = msgs.array()
+            if(messages[0]) {
+              var chan = messages[0].channel.name.split("-")
+              switch(chan[0]) {
+                case "cmd":
+                  {
+                    var cmd = chan[1]
+                    if(!discmd[cmd]) discmd[cmd] = {}
+                    console.log(`Received ${msgs.size} messages`)
+                    var i = messages.length
+                    while(i--) {
+                      var msg = messages[i].content.split("```")
+                      discmd[cmd][msg[0]] = msg[1].substring(msg[1].substring(0,10) == "javascript" ? 11 : 0)
+                    }
+                  } break;
 
-          })
-          .catch(console.error);
+                case "inc":
+                  {
+                    var cmd = chan[1]
+                    if(!discmd[cmd]) discmd[cmd] = {requires: {}}
+                    console.log(`Received ${msgs.size} messages`)
+                    var msg = messages[0].content.split("\n")
+                    for(var i in msg) {
+                      var code = msg[i].split(":")
+                      if(typeof includes.mod[code[1]] == "undefined") {
+                        console.log("New module required: " + code[1])
+                        includes.mod[code[1]] = require(code[1])
+                      }
+                      if(typeof includes.cmd[cmd] == "undefined") {
+                        console.log("Loaded new command reference table for command:", cmd)
+                        includes.cmd[cmd] = {}
+                      }
+                      console.log("New command module referenced:", code[1])
+                      includes.cmd[cmd][code[0]] = includes.mod[code[1]]
+                      console.log(typeof includes.cmd[cmd][code[0]], cmd, code[0], code[1])
+                    }
+                  } break;
+
+                default: {}
+              }
+            }
+            t++
+            if(t == j) {console.log("Ready to accept commands sir!"); ready = true}
+          }
+        )
+        .catch(console.error);
     }
   }
 });
@@ -64,36 +87,46 @@ client.on("disconnected", () => {
 });
 
 client.on("message", (msg) => {
-  //check if message is a command
-  if(msg.author.id != client.user.id && (msg.content.startsWith("!"))) {
-    console.log("treating " + msg.content + " from " + msg.author + " as command");
-    var cmdTxt = msg.content.split(" ")[0].substring(1);
-    var suffix = msg.content.substring(cmdTxt.length+2);//add one for the ! and one for the space
-    if(msg.isMentioned(client.user)){
-      try {
-        cmdTxt = msg.content.split(" ")[1];
-        suffix = msg.content.substring(client.user.mention().length+cmdTxt.length+2);
-      } catch(e){ //no command
-        msg.channel.send("Yes?");
-        return;
+  // make sure bot is ready to accept commands
+  if(ready) {
+    //check if message is a command
+    if(msg.author.id != client.user.id && (msg.content.startsWith("!"))) {
+      console.log("treating " + msg.content + " from " + msg.author + " as command");
+      var cmdTxt = msg.content.split(" ")[0].substring(1);
+      var suffix = msg.content.substring(cmdTxt.length+2);//add one for the ! and one for the space
+      if(msg.isMentioned(client.user)){
+        try {
+          cmdTxt = msg.content.split(" ")[1];
+          suffix = msg.content.substring(client.user.mention().length+cmdTxt.length+2);
+        } catch(e){ //no command
+          msg.channel.send("Yes?");
+          return;
+        }
       }
-    }
-    var cmd = discmd[cmdTxt];
+      var cmd = discmd[cmdTxt];
 
-    try {
-      safeEval(discmd[cmdTxt].callback, {
-        msg: msg,
-        client: client,
-        console: console,
-        requires: clone(discmd[cmdTxt].requires)
-      })
-    } catch(e) {
-      msg.reply("unfortunately, the developer that made that specific command made a mistake! Please contact him for assistance :)")
+      if(cmd) {
+        try {
+          msg.__rep = msg.reply
+          msg.reply = function(txt) {
+            this.__rep(txt).then(msg => console.log(`Sent message to ${msg.author}: ${txt}`)).catch(console.error)
+          }
+          console.log(includes.cmd[cmdTxt])
+          safeEval(discmd[cmdTxt].callback, {
+            msg: msg,
+            client: client,
+            console: console,
+            discordapi: Discord,
+            requires: clone(includes.cmd[cmdTxt])
+          })
+        } catch(e) {
+          msg.reply("unfortunately, the developer that made that specific command made a mistake! Please contact him for assistance :)")
+          console.log(e)
+        }
+      }
     }
   }
 });
 client.on("messageUpdate", (oldMessage, newMessage) => {
-  //console.log(oldMessage, newMessage);
 });
-
 client.login('your.api-key');
